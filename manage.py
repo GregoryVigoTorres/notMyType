fa2c3b437b9bd1552860307d9169280dea97d24e
Gregory Vigo Torres <gregory.vigo@gmail.com>
2016-06-04 16:19:29 +0200

    logging format

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ manager.add_command('fixfont', FixFont())
 manager.add_command('test', RunTests)
 
 if __name__ == '__main__':
-    logging.basicConfig(format='[%(levelname)s] %(lineno)d in %(funcName)s - {}%(message)s'.format(Fore.CYAN), level=logging.DEBUG)
+    logging.basicConfig(format='[notMyType][%(levelname)s] %(lineno)d in %(funcName)s - {}%(message)s'.format(Fore.CYAN), level=logging.DEBUG)
     manager.run()
 
 
